7e62fbcbe7391f67f5b89ac34867fb4dd27a27e5
Sergieo <sergieol23@gmail.com>
2022-06-27 22:22:40 -0500

    Updates

---
+++ precache-manifest.1cfd2f35c588d18e71299047790461c9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bf0d5ca128ba004d6c9",
+    "url": "/React-Portfolio/static/css/main.be6e4e09.chunk.css"
+  },
+  {
+    "revision": "4bf0d5ca128ba004d6c9",
+    "url": "/React-Portfolio/static/js/main.386362cd.chunk.js"
+  },
+  {
+    "revision": "2e3a11d94692a9f0dc15",
+    "url": "/React-Portfolio/static/js/runtime~main.e2a116d9.js"
+  },
+  {
+    "revision": "2d1d792894b30e92cce5",
+    "url": "/React-Portfolio/static/js/2.1d046d27.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/React-Portfolio/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "d27dce08bbbf00eed63337e1438a30ae",
+    "url": "/React-Portfolio/static/media/profile.d27dce08.jpg"
+  },
+  {
+    "revision": "dd5a8b442766991861d242426b19685b",
+    "url": "/React-Portfolio/static/media/0.dd5a8b44.jpg"
+  },
+  {
+    "revision": "dff6020aa9c9da395c285308c02f63c4",
+    "url": "/React-Portfolio/static/media/1.dff6020a.jpg"
+  },
+  {
+    "revision": "9a91ed14336af8e34c6b00a81409e917",
+    "url": "/React-Portfolio/static/media/2.9a91ed14.jpg"
+  },
+  {
+    "revision": "bc161a9494915894c054a0970d173ad3",
+    "url": "/React-Portfolio/static/media/3.bc161a94.jpg"
+  },
+  {
+    "revision": "88a973495cd87eecc422373365d1bb06",
+    "url": "/React-Portfolio/static/media/4.88a97349.jpg"
+  },
+  {
+    "revision": "f9ee67fe73c159f0802d8ec7b64034b8",
+    "url": "/React-Portfolio/static/media/5.f9ee67fe.jpg"
+  },
+  {
+    "revision": "a2a91d564bebdff2fc6b2b534b30d1e9",
+    "url": "/React-Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d667056dda1e8793d3adcdbc9b4adfa8.js"
+  "/React-Portfolio/precache-manifest.1cfd2f35c588d18e71299047790461c9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
