209a5dfa4353a88cfdf40537dccc5925640fe833
Sergieo <sergieol23@gmail.com>
2022-06-27 22:07:02 -0500

    Updates

---
+++ precache-manifest.d667056dda1e8793d3adcdbc9b4adfa8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bf0d5ca128ba004d6c9",
+    "url": "/static/css/main.be6e4e09.chunk.css"
+  },
+  {
+    "revision": "4bf0d5ca128ba004d6c9",
+    "url": "/static/js/main.386362cd.chunk.js"
+  },
+  {
+    "revision": "89835fe290a45d3e62a0",
+    "url": "/static/js/runtime~main.ba9f6ff0.js"
+  },
+  {
+    "revision": "2d1d792894b30e92cce5",
+    "url": "/static/js/2.1d046d27.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "d27dce08bbbf00eed63337e1438a30ae",
+    "url": "/static/media/profile.d27dce08.jpg"
+  },
+  {
+    "revision": "dd5a8b442766991861d242426b19685b",
+    "url": "/static/media/0.dd5a8b44.jpg"
+  },
+  {
+    "revision": "dff6020aa9c9da395c285308c02f63c4",
+    "url": "/static/media/1.dff6020a.jpg"
+  },
+  {
+    "revision": "9a91ed14336af8e34c6b00a81409e917",
+    "url": "/static/media/2.9a91ed14.jpg"
+  },
+  {
+    "revision": "bc161a9494915894c054a0970d173ad3",
+    "url": "/static/media/3.bc161a94.jpg"
+  },
+  {
+    "revision": "88a973495cd87eecc422373365d1bb06",
+    "url": "/static/media/4.88a97349.jpg"
+  },
+  {
+    "revision": "f9ee67fe73c159f0802d8ec7b64034b8",
+    "url": "/static/media/5.f9ee67fe.jpg"
+  },
+  {
+    "revision": "1a2f4aa6f1383010a8fdaa319a5132b9",
+    "url": "/index.html"
+  }
+];
